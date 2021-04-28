6070410d227736dfcf3839bee800c2bdfa3668a2
ryuman masanobu <22011376mr@tama.ac.jp>
2021-04-28 20:47:16 +0900

    ok

--- webserver.py
+++ webserver.py
@@ -1,25 +1,49 @@
 from cgi import FieldStorage
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+import re
+from html.parser import HTMLParser
+from bs4 import BeautifulSoup
 
 with open('index.html', 'r') as f:
     index_file = f.read()
 
-
+with open('registration.html','r') as f:
+    soup = BeautifulSoup('f.read', 'html.parser')
 
 
 class OriginalHTTPRequestHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
+        print(self.path)
+        if re.search('registration.html$', self.path) != None:
+            self.send_response(200)
+            self.end_headers()
 
-        self.send_response(200)
-        self.end_headers()
+            htmltag = soup.new_tag('html')
+            headtag = soup.new_tag("head")
+            bodytag = soup.new_tag("body")
+            sumple = soup.new_tag("h1")
+            sumple.string="slack"
+            htmltag.append(headtag)
+            htmltag.append(bodytag)
+            bodytag.append(sumple)
+
+            html=htmltag
+            soup
+            self.wfile.write(html.encode('UTF-8'))
 
 
-        self.wfile.write(index_file.encode('UTF-8'))
-        return None
 
-    
+            return None
+        # 200 コードとヘッダ終了コードを送ってから本文を送り出す
+        else:
+
+
+            super().do_GET()
+
+
+
 def run(server_class=HTTPServer, handler_class=OriginalHTTPRequestHandler):
-    server_address = ('', 8000)
+    server_address = ('', 8001)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
 
